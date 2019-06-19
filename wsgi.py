5ed069e2300721f81949e5b70f728b40bde19e07
Arnaud CHONE <arnaud_chone@hotmail.com>
2019-06-19 11:24:49 +0200

    Second deployment of Flask boilerplate

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
+""" Simple REST API """
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +8,10 @@ def hello():
     return "Hello Dude!"
 
 @app.route('/api/v1/products')
-def hello():
-    return "Hello Dude!"
+def product_to_json():
+    PRODUCTS = [
+        {'id': 1, 'name': 'Skello'},
+        {'id': 2, 'name': 'Socialive.tv'},
+        {'id': 3, 'name': 'Product3'}
+    ]
+    return jsonify(PRODUCTS)
